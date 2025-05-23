9258cbbaeb493cf25408f3f351a2dfc7efd748dd
shrutiramdurg <shrutisramdurg@gmail.com>
2025-05-23 10:32:13 +0530

    Solved Dice throw

---
+++ Dice throw.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int noOfWays(int m, int n, int x) {
+        vector<vector<int>> dp(n + 1, vector<int>(x + 1, 0));
+        dp[0][0] = 1;  // Base case
+        
+        for (int dice = 1; dice <= n; ++dice) {
+            for (int sum = 1; sum <= x; ++sum) {
+                for (int face = 1; face <= m; ++face) {
+                    if (sum - face >= 0)
+                        dp[dice][sum] += dp[dice - 1][sum - face];
+                }
+            }
+        }
+
+        return dp[n][x];
+    }
+};
